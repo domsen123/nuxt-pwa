9b3702ffe00c927f263be9067b14be426e30d099
domsen123 <dmarx@marxulm.de>
2026-03-13 14:23:22 +0100

    testing

--- app/components/Pwa/LinkCard.vue
+++ app/components/Pwa/LinkCard.vue
@@ -1,14 +1,18 @@
 <script lang="ts" setup>
-defineProps<{
+import type { ButtonProps } from '@nuxt/ui'
+
+withDefaults(defineProps<{
   title: string
-}>()
+  items?: ButtonProps[]
+}>(), {
+  items: () => [],
+})
 </script>
 
 <template>
-  <UCard variant="subtle" :ui="{ body: 'p-0 sm:p-0' }">
+  <UCard variant="soft" :ui="{ body: 'p-0 sm:p-0', root: 'rounded-none' }">
     <div class="px-4 py-2 text-muted font-medium text-sm" v-text="title"></div>
-    <UButton icon="i-lucide-form" label="Form Elements" block trailing-icon="i-lucide-chevron-right" variant="ghost" color="neutral" size="xl" />
-    <UButton icon="i-lucide-form" label="asdf" block trailing-icon="i-lucide-chevron-right" variant="ghost" color="neutral" size="xl" />
+    <UButton v-for="(item, idx) in items" :key="idx" block trailing-icon="i-lucide-chevron-right" variant="ghost" color="neutral" size="xl" v-bind="item" />
   </UCard>
 </template>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,7 +9,17 @@ async function handleRefresh(done: () => void) {
   <PullToRefresh @refresh="handleRefresh">
     <div class="space-y-4">
       <UContainer></UContainer>
-      <PwaLinkCard title="Konto" />
+      <PwaLinkCard
+        title="Wie du Intagram nutzt"
+        :items="[
+          { icon: 'i-lucide-bookmark', label: 'Gespeichert' },
+          { icon: 'i-lucide-clock-check', label: 'Archiv' },
+          { icon: 'i-lucide-clock-fading', label: 'Deine Aktivitäten' },
+          { icon: 'i-lucide-bell', label: 'Benachrichtigungen' },
+          { icon: 'i-lucide-bell', label: 'Benachrichtigungen' },
+          { icon: 'i-lucide-clock', label: 'Zeitmanagement' },
+        ]"
+      />
     </div>
   </PullToRefresh>
 </template>
